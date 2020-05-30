46fa6a4d296f60d2186dd4c1f32407737b2f6608
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-05-30 17:59:40 +0530

    remove spring boot starter web dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ repositories {
 dependencies {
 	implementation("org.apache.commons:commons-csv:1.8")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")

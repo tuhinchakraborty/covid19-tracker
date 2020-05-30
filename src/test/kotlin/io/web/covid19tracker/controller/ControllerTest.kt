3e1bf5ca991ced3958c99064cb016939adfb2f9a
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-05-30 17:45:55 +0530

    add tests for DataService

--- src/test/kotlin/io/web/covid19tracker/controller/ControllerTest.kt
+++ src/test/kotlin/io/web/covid19tracker/controller/ControllerTest.kt
@@ -1,5 +1,48 @@
 package io.web.covid19tracker.controller
 
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.every
+import io.mockk.mockk
+import io.web.covid19tracker.models.Data
+import io.web.covid19tracker.service.DataService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.model
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import reactor.core.publisher.Mono
 
-internal class ControllerTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension::class)
+internal class ControllerTest {
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+    private lateinit var mockMvc: MockMvc
+
+    private val dataService = mockk<DataService>()
+
+    @BeforeEach
+    internal fun setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+    }
+
+    @Disabled
+    @Test
+    internal fun `should show data`() {
+        val data = listOf(Data("province", "country", 200, 190))
+        every { dataService.fetchData() } returns Mono.just(data)
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect {
+            it.response.status == HttpStatus.OK.value()
+        }.andExpect(model().attributeExists("data"))
+    }
+}
--- src/test/kotlin/io/web/covid19tracker/service/DataServiceTest.kt
+++ src/test/kotlin/io/web/covid19tracker/service/DataServiceTest.kt
@@ -1,5 +1,66 @@
 package io.web.covid19tracker.service
 
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.web.covid19tracker.config.AppConfig
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
 
-internal class DataServiceTest
+internal class DataServiceTest {
+
+    @MockK
+    private lateinit var appConfig: AppConfig
+
+    @InjectMockKs
+    private lateinit var dataService: DataService
+
+    private lateinit var mockWebServer: MockWebServer
+
+    @BeforeEach
+    internal fun setUp() {
+        mockWebServer = MockWebServer()
+        mockWebServer.start()
+        val url = mockWebServer.url("/")
+        appConfig = AppConfig(url.toString(), "/data")
+        dataService = DataService(appConfig)
+        MockKAnnotations.init(this)
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    internal fun shouldFetchData() {
+        mockWebServer.enqueue(MockResponse()
+                .setResponseCode(200)
+                .setBody("Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20,1/25/20,1/26/20\n" +
+                        ",Afghanistan,33.0,65.0,100,118,124,130,136\n" +
+                        ",Germany,51.0,9.0,180,181,185,190,200\n"))
+
+        val fetchData = dataService.fetchData()
+        val dataFlux = fetchData.flatMapMany {
+            Flux.fromIterable(it)
+        }
+
+        StepVerifier.create(dataFlux)
+                .expectNextMatches {
+                    it.country == "Germany"
+                            && it.currentCount == 200
+                            && it.previousCount == 190
+                }
+                .expectNextMatches {
+                    it.country == "Afghanistan"
+                            && it.currentCount == 136
+                            && it.previousCount == 130
+                }
+                .verifyComplete()
+    }
+}

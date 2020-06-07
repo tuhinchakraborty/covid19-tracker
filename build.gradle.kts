ce8315a053297508b4a64ceb89e69a6939b291c2
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-06-07 18:10:30 +0530

    add vaadin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,36 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.5.RELEASE"
+	id("org.springframework.boot") version "2.3.0.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	id("com.google.cloud.tools.jib") version "1.8.0"
-	kotlin("jvm") version "1.3.61"
-	kotlin("plugin.spring") version "1.3.61"
+	kotlin("jvm") version "1.3.72"
+	kotlin("plugin.spring") version "1.3.72"
 	jacoco
+	id("com.google.cloud.tools.jib") version "1.8.0"
 }
 
-group = "io.web"
+group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
-
-val developmentOnly by configurations.creating
-configurations {
-	runtimeClasspath {
-		extendsFrom(developmentOnly)
-	}
-}
 
 repositories {
 	mavenCentral()
 }
 
+extra["vaadinVersion"] = "14.2.0"
+
 dependencies {
-	implementation("org.apache.commons:commons-csv:1.8")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.vaadin:vaadin-spring-boot-starter")
+	implementation("com.vaadin:vaadin-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.apache.commons:commons-lang3:3.9")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.apache.commons:commons-csv:1.8")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
@@ -42,6 +38,12 @@ dependencies {
 	testImplementation("io.mockk:mockk:1.9.3")
 	testImplementation("com.squareup.okhttp3:mockwebserver:3.11.0")
 	testImplementation("com.github.tomakehurst:wiremock-jre8:2.25.1")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+	}
 }
 
 tasks.withType<Test> {
@@ -52,17 +54,5 @@ tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
-	}
-}
-
-tasks.test {
-	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
-}
-tasks.jacocoTestReport {
-	dependsOn(tasks.test) // tests are required to run before generating the report
-	reports {
-		xml.isEnabled = true
-		csv.isEnabled = false
-		html.isEnabled = true
 	}
 }

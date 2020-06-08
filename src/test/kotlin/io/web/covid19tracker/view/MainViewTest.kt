d1a5531dca277fc75fff1c0bb5841ad9f5fe8bb5
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-06-08 17:23:56 +0530

    add tests for main view

---
+++ src/test/kotlin/io/web/covid19tracker/view/MainViewTest.kt
@@ -0,0 +1,51 @@
+package io.web.covid19tracker.view
+
+import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.combobox.ComboBox
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.mockk
+import io.web.covid19tracker.service.DataService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class MainViewTest {
+    private val dataService: DataService = mockk()
+
+    @InjectMockKs
+    private lateinit var mainView: MainView
+
+    @BeforeEach
+    internal fun setUp() {
+        every { dataService.getCountryNames() } returns listOf("Country One", "Country Two")
+        mainView = MainView(dataService)
+    }
+
+    @Test
+    fun shouldHaveVerticalLayout() {
+        val firstComponent = mainView.getComponentAt(0)
+
+        assertEquals(1, mainView.componentCount)
+        assertEquals(VerticalLayout::class, firstComponent::class)
+    }
+
+    @Test
+    fun shouldHaveComboBoxAndButtonWithinVerticalLayout() {
+        val verticalLayout = mainView.getComponentAt(0)
+        assertEquals(2, verticalLayout.children.count())
+
+        val components = mutableListOf<Component>()
+        verticalLayout.children.forEachOrdered {
+            components.add(it)
+        }
+
+        assertEquals(ComboBox::class, components[0]::class)
+        assertEquals(Button::class, components[1]::class)
+        assertTrue(components[0].isVisible)
+        assertTrue(components[1].isVisible)
+    }
+}
